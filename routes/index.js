d2adb910ec9de1a39c241275d16cc5c9253e3f52
Gagan <gagan3809@gmail.com>
2023-04-21 17:20:01 +0530

    create another controller and installed ejs

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
-
-const homeController = require('../controllers/home_controller');
-
 const router = express.Router();
+const homeController = require('../controllers/home_controller');
 
 console.log('Router loaded');
 
 router.get('/',homeController.home);
+router.use('/users',require('./users'));
+
+//for any further routes, access from here
+//router.use('/router_name',require('./routerfile'));
+
+
 
 module.exports = router;
